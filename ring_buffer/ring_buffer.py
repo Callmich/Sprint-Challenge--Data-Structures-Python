4d64dc91d66d2714c6417e194a9f6952e94c8ceb
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-17 09:34:07 -0600

    added in a counter to account for the index and when to have it run

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,13 +2,18 @@ class RingBuffer:
     def __init__(self, capacity):
         self.data = []
         self.capacity = capacity
+        self.run = 0
 
     def append(self, item):
         if len(self.data) < self.capacity:
             self.data.append(item)
         else:
-            self.data.pop(0)
-            self.data.append(item)
+            self.data.pop(self.run)
+            self.data.insert(self.run, item)
+            if self.run < self.capacity - 1:
+                self.run += 1
+            else:
+                self.run = 0
 
     def get(self):
         return self.data
