e74b9eb214834d23e6a6600d308e28e04a0aeb64
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-17 11:27:51 -0600

    switched the order of the list of which is a tree and which is the check. I think it is a little faster

--- names/names.py
+++ names/names.py
@@ -20,7 +20,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-# Create a tree node
+# Create a tree node - I guess this is actually a Binary tree search.
 class TreeNode:
     def __init__(self, value):
         self.value = value
@@ -55,16 +55,15 @@ class TreeNode:
 
 # create a binary search tree with one of the lists
 
-nameTree = TreeNode(names_1[0])
+nameTree = TreeNode(names_2[0])
 
-for n in names_1:
+# check if the value of one of the other list names matches a item in the tree if so then push it into the dupes
+for n in names_2:
     nameTree.insert(n)
-for check in names_2:
+for check in names_1:
     if nameTree.contains(check):
         duplicates.append(check)
 
-
-# check if the value of one of the other list names matches a item in the tree if so then push it into the dupes
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
